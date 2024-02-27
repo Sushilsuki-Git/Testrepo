7fe860489ffaa64bc0e2731763dfaac0b0274b46
Sushilsuki-Git <sushiljp000@gmail.com>
2024-02-27 08:42:23 +0530

    new file

---
+++ greatest.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+    int p, q, r;
+    printf("enter 3 numbers to find the greatest:");
+    scanf("%d%d%d", &p, &q, &r);
+    if(p>=q && p>=r) {
+        printf("the greatest number p : %d", p);
+    } else if(q >= r) {
+         printf("the greatest number q : %d", q);
+    } else {
+         printf("the greatest number r : %d", r);
+    }
+    return 0;
+}
